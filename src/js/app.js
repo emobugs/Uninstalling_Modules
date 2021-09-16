ea8dbdafac2086c15c9c2967f287bb228c7274d8
martin pandarski <martin.pandarski@boom.dev>
2021-09-16 15:56:36 +0300

    feat: add boilerplate for task-86

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import "../scss/app.scss";
-
+const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  const ul = document.querySelector("ul");
+  const numbersArray = [1, 3, 4, 5, 6, 7];
+  const isEven = (n) => n % 2 === 0;
+  const filteredArray = R.filter(isEven, numbersArray);
 });
